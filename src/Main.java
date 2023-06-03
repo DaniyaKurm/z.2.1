bdb499af3a66511645daadfd8435b6ecddcefad3
Daniya <daniya.kurmakaeva@yandex.ru>
2023-06-03 21:11:08 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int amount = 7200;
+        int x = 20;
+        int bonus = amount / x;
+
+        System.out.println (bonus);
+        
+
 
         }
     }
